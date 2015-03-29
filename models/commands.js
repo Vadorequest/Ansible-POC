dd00a88d509ced3b1b1194eecb491eb6a14c27d4
Vadorequest <ambroise.dhenain@gmail.com>
2015-03-29 22:02:27 +0200

    Some changes, moving the working directory so the command works for Mark.

--- models/commands.js
+++ models/commands.js
@@ -1,14 +1,19 @@
 // Import global functions for logging using colors.
 require('./logger');
-
+var shell = require('shelljs');
 var AnsiblePlaybookCli = require('./AnsiblePlaybookCli').AnsiblePlaybookCli;
 
+// Move to the right folder.
+shell.cd('/home/vagrant/ansible-nxos/test');
+console.info('Current location is now:' + shell.pwd());// Echo the current location.
+
+// Execute Ansible command from the right directory.
 AnsiblePlaybookCli.exec([
     '-v',
     '-M ../library',
     '-i inventory',
     "-e '@credentials.yml'",
-    '../test.yml'
+    'test.yml'
 ], function(message, command){
     console.success(message, command);
 }, function(message, command, error){
