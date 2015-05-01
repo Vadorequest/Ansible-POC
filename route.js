cffc34b1ab394e448ce10bed9b235d1358e28895
Vadorequest <ambroise.dhenain@gmail.com>
2015-05-01 17:06:51 +0200

    Add route.

---
+++ route.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var nodemailer = require('nodemailer')
+var controller = require('./controller')
+var model = require('./model')
+
+module.exports = function(server, config, env, log) {
+  var auth = model.auth(config, env)
+  server.get('/', controller.api.getRoot)
+  server.head('/', controller.api.getRoot)
+  server.get('/api', controller.api.get)
+  server.head('/api', controller.api.get)
+  server.get('/api/v1', controller.api.getV1)
+  server.head('/api/v1', controller.api.getV1)
+
+
+  server.post('/api/experimental/vlans'
+    , model.vlans.cli()
+    , controller.vlans.postAll())
+  server.head('/api/experimental/vlans'
+    , model.vlans.cli()
+    , controller.vlans.postAll())
+
+}
+
+  
