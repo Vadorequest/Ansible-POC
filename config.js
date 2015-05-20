bc360248076b311b5f53a82218384b1cc42e8dfd
Vadorequest <ambroise.dhenain@gmail.com>
2015-05-20 21:27:02 +0200

    Change config.

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    env: 'dev',// dev || prod
+    env: 'dev',// dev || prod || rct
     default_ansible_filename: 'test',
     prod: {
-        ansible_path: '/local/mnt/ansible_nxos/test/'
+        ansible_path: '/local/mnt/ansible_roles/ansible-nxos/test'
     },
     dev: {
         ansible_path: './yaml/'
+    },
+    rct: {
+        ansible_path: '/local/mnt/ansible_roles/ansible-nxos/test'
     }
 };
