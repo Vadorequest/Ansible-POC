8914e11f6be355e5335cde306b1532232272ab08
Vadorequest <ambroise.dhenain@gmail.com>
2015-05-21 22:20:33 +0200

    Use mkdirp node module to create recursively all folders. Creates host_vars and group_vars folder and put the expected files in these two folders.

--- config.js
+++ config.js
@@ -2,12 +2,14 @@ module.exports = {
     env: 'dev',// dev || prod || rct
     default_ansible_filename: 'test',
     prod: {
-        ansible_path: '/local/mnt/ansible_roles/ansible-nxos/test'
+        ansible_path: '/local/mnt/ansible_roles/ansible-nxos/test/'
     },
     dev: {
-        ansible_path: './yaml/'
+        ansible_path: './.tmp_ansible_nxos/test/'
     },
     rct: {
-        ansible_path: '/local/mnt/ansible_roles/ansible-nxos/test'
-    }
+        ansible_path: '/local/mnt/ansible_roles/ansible-nxos/test/'// TODO group_vars/test.yml + host_vars folders
+    },
+    group_vars_dirname: 'group_vars',
+    host_vars_dirname: 'host_vars'
 };
